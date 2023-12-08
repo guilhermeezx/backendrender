4c100108ae4b9185867ad2815472c59e6d7993c8
guilherme <88243712+guilhermeezx@users.noreply.github.com>
2023-12-08 10:18:38 -0300

    2

--- banco.js
+++ banco.js
@@ -2,10 +2,7 @@ const sequelize = require("sequelize");
 require('dotenv').config()
 
 const conexao = new sequelize(
-    process.env.DB_HOST,
-    {
-        dialect:"postgres",
-    }
+    process.env.DB_HOST,{dialect:"postgres",}
 )
 
 module.exports = { conexao }
